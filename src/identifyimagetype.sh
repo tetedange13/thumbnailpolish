6fa3a1eac5da391d311e58d35cf5d3b8e84195d8
tetedange13 <felix.deslacs@gmail.com>
2020-07-23 15:03:10 +0200

    Add files via upload
    Added type "MISEQ2" (larger img, specific to DPNI) + typo changes

--- src/identifyimagetype.sh
+++ src/identifyimagetype.sh
@@ -3,57 +3,51 @@
 # creates a file in the current directory called "type" whose 
 # contents are a symbol indicating the image-composite recipe
 
+
 f=$( ls L001/C1.1/s*[aA].jpg L001/C1.1/s*red.jpg | head -n 1)
-if [[ "$( identify $f )" =~ .*608x300.* ]] 
-    then 
+if [[ "$( identify $f )" =~ .*608x300.* ]] ; then 
     echo images in MISEQ format 
     echo MISEQ > thumbnailpolish.type
-elif [[ "$( identify $f )" =~ .*496x450.* ]] 
-    then 
+elif [[ "$( identify $f )" =~ .*661x300.* ]] ; then 
+    echo images in MISEQ2 format 
+    echo MISEQ2 > thumbnailpolish.type
+elif [[ "$( identify $f )" =~ .*496x450.* ]] ; then 
     echo images in HISEQ format
     echo HISEQ > thumbnailpolish.type
-elif [[ "$( identify $f )" =~ .*542x450.* ]] 
-    then 
+elif [[ "$( identify $f )" =~ .*542x450.* ]] ; then 
     echo images in HISEQ2 format
     echo HISEQ2 > thumbnailpolish.type
-elif [[ "$( identify $f )" =~ .*576x300.* ]] 
-    then 
+elif [[ "$( identify $f )" =~ .*576x300.* ]] ; then 
     echo images in GAII format
     echo GAII > thumbnailpolish.type 
-elif [[ "$( identify $f )" =~ .*700x300.* ]] 
-    then 
+elif [[ "$( identify $f )" =~ .*700x300.* ]] ; then 
     echo images in NEXTSEQ format
     echo NEXTSEQ > thumbnailpolish.type 
+else
+    echo "UNKNOWN TYPE (based on dimensions if img)" 
 fi
 
-if [[ -e "L001/C1.1/s_1_1112_A.jpg" ]] 
-    then
+if [[ -e "L001/C1.1/s_1_1112_A.jpg" ]] ; then
     echo tree looks like HISEQ2 
     echo HISEQ2  > thumbnailpolish.tree
-elif [[ -e "L001/C1.1/s_1_1101_A.jpg" ]]
-    then
+elif [[ -e "L001/C1.1/s_1_1101_A.jpg" ]] ; then
     echo tree looks like HISEQ 
     echo HISEQ  > thumbnailpolish.tree
-elif [[ -e "L001/C1.1/s_1_99_a.jpg" ]]
-    then
+elif [[ -e "L001/C1.1/s_1_99_a.jpg" ]] ; then
     echo tree looks like GAII 
     echo GAII > thumbnailpolish.tree
-elif [[ -e "L001/C1.1/s_1_1_a.jpg" ]]
-    then
+elif [[ -e "L001/C1.1/s_1_1_a.jpg" ]] ; then
     echo Tree looks like MISEQ1
     echo MISEQ1 > thumbnailpolish.tree
-elif [[ -e "L001/C1.1/s_1_1114_a.jpg" ]]
-    then
+elif [[ -e "L001/C1.1/s_1_1114_a.jpg" ]] ; then
     echo Tree looks like MISEQ2
     echo MISEQ2 > thumbnailpolish.tree
-elif [[ -e "L001/C1.1/s_1_11101_red.jpg" ]]
-    then
+elif [[ -e "L001/C1.1/s_1_11101_red.jpg" ]] ; then
     echo Tree looks like NEXTSEQ
     echo NEXTSEQ > thumbnailpolish.tree
 fi
 
-for i in {1..600}
-    do
+for i in {1..600} ; do
     if [[ ! -d "L001/C$i.1" ]]
         then
         break 
